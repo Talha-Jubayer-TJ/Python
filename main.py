f5682c3a73063947e5b8e3623e39e1db435f14b7
Talha_Jubayer <tlhajubayer321@gmail.com>
2024-03-31 01:45:56 +0600

    first program

---
+++ main.py
@@ -0,0 +1,9 @@
+# First Program
+num1 = 5
+num2 = 5.5
+sum = num1 + num2
+
+print(sum)
+print("Hello, This is my First Python Program.\nI'm trying to learn programming from the very basic.\nWish me luck!  ")
+print('Hello World!!')
+print(4+6)
